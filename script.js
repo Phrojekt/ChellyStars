d483430a6f474362d84f20024cc4830db40d3318
Phrojekt <101226915+Phrojekt@users.noreply.github.com>
2022-07-12 19:52:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const XiaoModal = {
+    open(){
+        document
+            .querySelector('.ModalOverlay1')
+            .classList
+            .add('active')
+        },
+        close(){
+            document
+                .querySelector('.ModalOverlay1')
+                .classList
+                .remove('active')
+        }
+}
+
+const HuTaoModal = {
+    open(){
+        document
+            .querySelector('.ModalOverlay2')
+            .classList
+            .add('active')
+        },
+        close(){
+            document
+                .querySelector('.ModalOverlay2')
+                .classList
+                .remove('active')
+        }
+}
+
+const ZhongliModal = {
+    open(){
+        document
+            .querySelector('.ModalOverlay3')
+            .classList
+            .add('active')
+        },
+        close(){
+            document
+                .querySelector('.ModalOverlay3')
+                .classList
+                .remove('active')
+        }
+}
+
+const ThomaModal = {
+    open() {
+        document
+            .querySelector('.ModalOverlay4')
+            .classList
+            .add('active')
+    },
+    close(){
+        document
+            .querySelector('.ModalOverlay4')
+            .classList
+            .remove('active')
+    }
+}
+
+const RaidenModal = {
+    open() {
+        document
+            .querySelector('.ModalOverlay5')
+            .classList
+            .add('active')
+    },
+    close(){
+        document
+            .querySelector('.ModalOverlay5')
+            .classList
+            .remove('active')
+    }
+}
